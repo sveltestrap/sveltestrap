e610f2b735a546531fcda731da4552541a372af3
marcomauricio1 <marco.mauricio@opensense.com>
2023-12-11 13:14:53 -0800

    stories(docs): add Table JSDoc comments

--- src/Table/Table.svelte
+++ src/Table/Table.svelte
@@ -6,13 +6,53 @@
   import { TableHeader } from '../TableHeader';
 
   let className = '';
+  /**
+   * Represents a custom class name for the component.
+   * @type {string}
+   */
+
   export { className as class };
+
+  /**
+   * Represents the size of the component.
+   * @type {string}
+   */
   export let size = '';
+
+  /**
+   * Indicates whether the component should have a bordered style.
+   * @type {boolean}
+   */
   export let bordered = false;
+
+  /**
+   * Indicates whether the component should have a borderless style.
+   * @type {boolean}
+   */
   export let borderless = false;
+
+  /**
+   * Indicates whether the component should have a striped style.
+   * @type {boolean}
+   */
   export let striped = false;
+
+  /**
+   * Indicates whether the component should have a hover effect.
+   * @type {boolean}
+   */
   export let hover = false;
+
+  /**
+   * Indicates whether the component should be responsive.
+   * @type {boolean}
+   */
   export let responsive = false;
+
+  /**
+   * Represents the number of rows for the component.
+   * @type {Object[]}
+   */
   export let rows = undefined;
 
   $: classes = classnames(
