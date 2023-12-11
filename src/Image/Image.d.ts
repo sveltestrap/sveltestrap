a1367de139b1fca7d9d3353dcb400754c81f08b4
marcomauricio1 <marco.mauricio@opensense.com>
2023-12-11 11:14:26 -0800

    stories(docs): update Image ts

--- src/Image/Image.d.ts
+++ src/Image/Image.d.ts
@@ -1,10 +1,17 @@
-import { SvelteComponent } from 'svelte';
+declare module 'svetlestrap' {
+  import { SvelteComponent } from 'svelte';
+  import { HTMLImgAttributes } from 'svelte/elements';
 
-export interface ImageProps
-  extends svelte.JSX.HTMLAttributes<HTMLElementTagNameMap['img']> {
-  figure?: boolean;
-  fluid?: boolean;
-  thumbnail?: boolean;
+  export interface ImageProps extends HTMLImgAttributes {
+    alt?: string;
+    figure?: boolean;
+    fluid?: boolean;
+    thumbnail?: boolean;
+  }
+
+  export interface ImageEvents {}
+
+  export interface ImageSlots {}
+
+  export default class Image extends SvelteComponent<ImageProps, ImageEvents, ImageSlots> {}
 }
-
-export default class Image extends SvelteComponent<ImageProps, {}, {}> {}
