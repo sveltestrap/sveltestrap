c137a06ac47f94121ec1864151a82245344807e2
marcomauricio1 <marco.mauricio@opensense.com>
2023-12-12 11:10:27 -0800

    stories(docs): add Container stories

---
+++ src/Container/Container.stories.svelte
@@ -0,0 +1,80 @@
+<script context="module">
+  import Container from './Container.svelte';
+
+  export const meta = {
+    title: 'Stories/Container',
+    component: Container,
+    parameters: {},
+    argTypes: {
+      class: {
+        className: 'string',
+        table: { disable: true }
+      },
+      sm: {
+        control: 'boolean'
+      },
+      md: {
+        control: 'boolean'
+      },
+      lg: {
+        control: 'boolean'
+      },
+      xl: {
+        control: 'boolean'
+      },
+      xxl: {
+        control: 'boolean'
+      },
+      fluid: {
+        control: 'boolean'
+      }
+    },
+    args: {
+      sm: true,
+      md: true,
+      lg: true,
+      xl: true,
+      xxl: true,
+      fluid: true
+    }
+  };
+</script>
+
+<script>
+  import { Story, Template } from '@storybook/addon-svelte-csf';
+</script>
+
+<Template let:args>
+  <Container {...args}>
+    <h3 class="container">fluid</h3>
+  </Container>
+</Template>
+
+<Story name="Basic">
+  <div class="container-wrapper">
+    <Container fluid>
+      <h3 class="container">fluid</h3>
+    </Container>
+    <div class="contained">
+      <Container sm>
+        <h3 class="container">sm</h3>
+      </Container>
+
+      <Container sm>
+        <h3 class="container">md</h3>
+      </Container>
+
+      <Container lg>
+        <h3 class="container">lg</h3>
+      </Container>
+
+      <Container xl>
+        <h3 class="container">xl</h3>
+      </Container>
+
+      <Container xxl>
+        <h3 class="container">xxl</h3>
+      </Container>
+    </div>
+  </div>
+</Story>
