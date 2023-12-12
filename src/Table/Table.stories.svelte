7db7fcc9c4e2622042c5deb862e483465e4b4a3d
Kieran Boyle <norcal.kieran@gmail.com>
2023-12-11 20:50:20 -0800

    documentation(table): fix basic example source

--- src/Table/Table.stories.svelte
+++ src/Table/Table.stories.svelte
@@ -55,8 +55,6 @@
   import { Story, Template } from '@storybook/addon-svelte-csf';
   import { Column } from 'sveltestrap';
 
-  const sizes = ['sm', 'lg'];
-
   const ROWS = [
     {
       first: 'Rufus',
@@ -119,6 +117,37 @@
       dob: new Date(1984, 6, 1)
     }
   ];
+
+  const basicSource = `<Table>
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>First Name</th>
+      <th>Last Name</th>
+      <th>Username</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</Table>`
 </script>
 
 <Template let:args>
@@ -174,7 +203,7 @@
   </Table>
 </Template>
 
-<Story name="Basic" />
+<Story name="Basic" source={basicSource} />
 
 <Story name="Bordered" args={{ bordered: true }} />
 
