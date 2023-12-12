12ebeed0cd751cf3c758aaa04de859d351ec6db1
marcomauricio1 <marco.mauricio@opensense.com>
2023-12-12 15:04:35 -0800

    refactor(types): update Progress ts

--- src/Progress/Progress.d.ts
+++ src/Progress/Progress.d.ts
@@ -1,20 +1,24 @@
-import { SvelteComponent } from 'svelte';
-import { Color } from '../shared';
+declare module 'sveltestrap' {
+  import { SvelteComponent } from 'svelte';
+  import { Color } from '../shared';
+  import { HTMLAttributes } from 'svelte/elements';
 
-export interface ProgressProps
-  extends svelte.JSX.HTMLAttributes<HTMLElementTagNameMap['div']> {
-  animated?: boolean;
-  bar?: boolean;
-  barClassName?: string;
-  color?: Color;
-  max?: string | number;
-  multi?: boolean;
-  striped?: boolean;
-  value?: string | number;
+  export interface ProgressProps extends HTMLAttributes<HTMLDivElement> {
+    animated?: boolean;
+    bar?: boolean;
+    barClassName?: string;
+    color?: Color;
+    max?: string | number;
+    multi?: boolean;
+    striped?: boolean;
+    value?: string | number;
+  }
+
+  export interface ProgressEvents {}
+
+  export interface ProgressSlots {
+    default: {};
+  }
+
+  export default class Progress extends SvelteComponent<ProgressProps, ProgressEvents, ProgressSlots> {}
 }
-
-export default class Progress extends SvelteComponent<
-  ProgressProps,
-  {},
-  { default: {} }
-> {}
