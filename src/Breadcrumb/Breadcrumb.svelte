26a51b545bdf4a261c54e2dba85df26942e42432
Kieran Boyle <norcal.kieran@gmail.com>
2023-12-12 15:17:46 -0800

    docs(breadcrumb): add breadcrumb stories and documentation

--- src/Breadcrumb/Breadcrumb.svelte
+++ src/Breadcrumb/Breadcrumb.svelte
@@ -1,17 +1,40 @@
 <script>
+  // Importing a utility function for handling class names.
   import { classnames } from '../utils';
 
+  /**
+   * Additional CSS class names for the <nav> element.
+   * @type {string}
+   */
   let className = '';
   export { className as class };
-  export let children = undefined;
-  export let divider = undefined;
+
+  /**
+   * The content to be displayed within the breadcrumb.
+   * @type {string}
+   */
+  export let children;
+
+  /**
+   * Custom divider character or string for the breadcrumb items.
+   * @type {string}
+   */
+  export let divider = '';
+
+  /**
+   * Additional CSS class names for the <ol> element inside the breadcrumb.
+   * @type {string}
+   */
   export let listClassName = '';
-  export let style = undefined;
+
+  /**
+   * Inline styles for the breadcrumb.
+   * @type {string}
+   */
+  export let style = '';
 
   $: listClasses = classnames('breadcrumb', listClassName);
-  $: styles = divider
-    ? `--bs-breadcrumb-divider: '${divider}'; ${style || ''}`
-    : style;
+  $: styles = divider ? `--bs-breadcrumb-divider: '${divider}'; ${style || ''}` : style;
 </script>
 
 <nav style={styles} {...$$restProps} class={className}>
