f60d0095cf76231211db9e73615454b7fa4a3340
marcomauricio1 <marco.mauricio@opensense.com>
2023-12-14 13:03:02 -0800

    stories(docs): add InputGroup JSDoc comments

--- src/InputGroup/InputGroup.svelte
+++ src/InputGroup/InputGroup.svelte
@@ -1,15 +1,21 @@
 <script>
   import { classnames } from '../utils';
 
+  // Additional CSS class name for the component
   let className = '';
+  /**
+   * Additional CSS class name for the component
+   * @type {string}
+   */
   export { className as class };
+
+  /**
+   * Size of the component.
+   * @type {string}
+   */
   export let size = '';
 
-  $: classes = classnames(
-    className,
-    'input-group',
-    size ? `input-group-${size}` : null
-  );
+  $: classes = classnames(className, 'input-group', size ? `input-group-${size}` : null);
 </script>
 
 <div {...$$restProps} class={classes}>
