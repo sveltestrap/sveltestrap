9da081863f5015784c55c150c82f957433d6e518
marcomauricio1 <marco.mauricio@opensense.com>
2023-12-14 12:58:02 -0800

    stories(types): update FormGroup ts

--- src/FormGroup/FormGroup.d.ts
+++ src/FormGroup/FormGroup.d.ts
@@ -1,20 +1,25 @@
-import { SvelteComponent } from 'svelte';
+declare module 'sveltestrap' {
+  import { SvelteComponent } from 'svelte';
+  import { HTMLAttributes } from 'svelte/elements';
 
-export interface FormGroupProps
-  extends svelte.JSX.HTMLAttributes<HTMLElementTagNameMap['div']> {
-  check?: boolean;
-  disabled?: boolean;
-  floating?: boolean;
-  id?: string;
-  inline?: boolean;
-  label?: string;
-  row?: boolean;
-  tag?: 'div' | 'fieldset';
+  export interface FormGroupProps extends HTMLAttributes<HTMLDivElement> {
+    check?: boolean;
+    disabled?: boolean;
+    floating?: boolean;
+    id?: string;
+    inline?: boolean;
+    label?: string;
+    row?: boolean;
+    tag?: 'div' | 'fieldset';
+  }
+
+  export interface FormGroupEvents {}
+
+  export interface FormGroupSlots {
+    default: {};
+    label: {};
+  }
+
+  declare class FormGroup extends SvelteComponent<FormGroupProps, FormGroupEvents, FormGroupSlots> {}
+  export default FormGroup;
 }
-
-declare class FormGroup extends SvelteComponent<
-  FormGroupProps,
-  {},
-  { default: {}; label: {} }
-> {}
-export default FormGroup;
