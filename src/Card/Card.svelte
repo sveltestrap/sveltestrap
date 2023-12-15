89492110012cf8f53a97c447e227650bfa5bfa6a
marcomauricio1 <marco.mauricio@opensense.com>
2023-12-15 14:27:34 -0800

    stories(doc): add Card JSDoc comments

--- src/Card/Card.svelte
+++ src/Card/Card.svelte
@@ -1,11 +1,36 @@
 <script>
   import { classnames } from '../utils';
 
+  // Additional CSS class names to add to the container.
   let className = '';
+
+  /**
+   * Exports a prop `class` which can be used to apply custom CSS classes.
+   * @type {string}
+   */
   export { className as class };
+  /**
+   * Determines whether to include a card body.
+   * @type {boolean}
+   */
   export let body = false;
+
+  /**
+   * Specifies the color theme of the card.
+   * @type {string}
+   */
   export let color = '';
+
+  /**
+   * Inverts the text color of the card.
+   * @type {boolean}
+   */
   export let inverse = false;
+
+  /**
+   * Render the card with an outline style.
+   * @type {boolean}
+   */
   export let outline = false;
 
   $: classes = classnames(
