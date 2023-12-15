db51ef1ac9b427f010c65e5abbd2b1c3492d0969
marcomauricio1 <marco.mauricio@opensense.com>
2023-12-15 14:29:44 -0800

    stories(docs): add Toast JSDoc comments

--- src/Toast/Toast.svelte
+++ src/Toast/Toast.svelte
@@ -7,15 +7,64 @@
 
   const dispatch = createEventDispatcher();
 
+  // Additional CSS class names to add to the container.
   let className = '';
+
+  /**
+   * Exports a prop `class` which can be used to apply custom CSS classes.
+   * @type {string}
+   */
   export { className as class };
+  /**
+   * Controls whether the Toast component autohides after a certain duration.
+   * @type {boolean}
+   */
   export let autohide = false;
+
+  /**
+   * Specifies whether the Toast component includes a body content.
+   * @type {boolean}
+   */
   export let body = false;
+
+  /**
+   * The time delay (in milliseconds) before the Toast component autohides.
+   * @type {number}
+   * @default 5000
+   */
   export let delay = 5000;
+
+  /**
+   * The duration (in milliseconds) for the fade-in and fade-out animation of the Toast component.
+   * @type {number}
+   * @default 200
+   */
   export let duration = 200;
+
+  /**
+   * Controls whether the Toast component fades in and out.
+   * @type {boolean}
+   * @default true
+   */
   export let fade = true;
+
+  /**
+   * Specifies the header content of the Toast component.
+   * @type {undefined}
+   */
   export let header = undefined;
+
+  /**
+   * Controls whether the Toast component is initially open.
+   * @type {boolean}
+   * @default true
+   */
   export let isOpen = true;
+
+  /**
+   * Function to toggle the visibility of the Toast component.
+   * @type {null}
+   */
   export let toggle = null;
 
   let timeout;
