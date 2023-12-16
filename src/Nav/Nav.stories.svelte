48c05f12647c6e6639d36151c4761c3b4e4d7b86
Kieran Boyle <norcal.kieran@gmail.com>
2023-12-16 12:45:46 -0800

    docs(nav): add nav stories

---
+++ src/Nav/Nav.stories.svelte
@@ -0,0 +1,247 @@
+<script context="module">
+  import Nav from './Nav.svelte';
+
+  export const meta = {
+    title: 'Stories/Nav',
+    component: Nav,
+    parameters: {},
+    argTypes: {
+      card: {
+        control: 'boolean'
+      },
+      class: {
+        control: false,
+        table: {
+          disable: true
+        }
+      },
+      fill: {
+        control: 'boolean'
+      },
+      horizontal: {
+        control: ''
+      },
+      navbar: {
+        control: 'boolean'
+      },
+      pills: {
+        control: 'boolean'
+      },
+      tabs: {
+        control: 'boolean'
+      },
+      underline: {
+        control: 'boolean'
+      },
+      vertical: {
+        control: {
+          type: 'select'
+        },
+        options: ['', 'xs', 'sm', 'md', 'lg', 'xl']
+      }
+    },
+    args: {
+      card: false,
+      class: '',
+      fill: false,
+      horizontal: '',
+      justified: false,
+      navbar: false,
+      pills: false,
+      tabs: false,
+      underline: false,
+      vertical: ''
+    }
+  };
+</script>
+
+<script>
+  import { Story, Template } from '@storybook/addon-svelte-csf';
+
+  import { NavItem, Dropdown, DropdownItem, DropdownToggle, DropdownMenu, NavLink } from '@sveltestrap/sveltestrap';
+
+  let isOpen = false;
+
+  let basicSource = `<script lang="ts">
+  import { Nav, NavItem, NavLink } from '@sveltestrap/sveltestrap';
+<\/script>
+
+<p>List Based</p>
+
+<Nav>
+  <NavItem active>
+    <NavLink href="#">Link</NavLink>
+  </NavItem>
+  <NavItem>
+    <NavLink href="#">Link</NavLink>
+  </NavItem>
+  <NavItem>
+    <NavLink href="#">Another Link</NavLink>
+  </NavItem>
+  <NavItem>
+    <NavLink disabled href="#">Disabled Link</NavLink>
+  </NavItem>
+</Nav>
+
+<hr />
+
+<p>Link Based</p>
+
+<Nav>
+  <NavLink href="#" active>Link</NavLink>
+  <NavLink href="#">Link</NavLink>
+  <NavLink href="#">Another Link</NavLink>
+  <NavLink disabled href="#">Disabled Link</NavLink>
+</Nav>`;
+</script>
+
+<Story name="Basic" let:args source={basicSource}>
+  <div class="nav-example">
+    <p>List Based</p>
+
+    <Nav {...args} color="dark">
+      <NavItem active>
+        <NavLink href="#">Link</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="#">Link</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="#">Another Link</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink disabled href="#">Disabled Link</NavLink>
+      </NavItem>
+    </Nav>
+
+    <hr />
+
+    <p>Link Based</p>
+
+    <Nav {...args} color="dark">
+      <NavLink href="#" active>Link</NavLink>
+      <NavLink href="#">Link</NavLink>
+      <NavLink href="#">Another Link</NavLink>
+      <NavLink disabled href="#">Disabled Link</NavLink>
+    </Nav>
+  </div>
+</Story>
+
+<Story name="Tabs">
+  <div class="nav-example">
+    <Nav tabs>
+      <NavItem>
+        <NavLink href="#" active>Link</NavLink>
+      </NavItem>
+      <Dropdown nav {isOpen} toggle={() => (isOpen = !isOpen)}>
+        <DropdownToggle nav caret>Dropdown</DropdownToggle>
+        <DropdownMenu>
+          <DropdownItem header>Header</DropdownItem>
+          <DropdownItem disabled>Action</DropdownItem>
+          <DropdownItem>Another Action</DropdownItem>
+          <DropdownItem divider />
+          <DropdownItem>Another Action</DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
+      <NavItem>
+        <NavLink href="#">Link</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="#">Another Link</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink disabled href="#">Disabled Link</NavLink>
+      </NavItem>
+    </Nav>
+  </div>
+</Story>
+
+<Story name="Pills">
+  <div class="nav-example">
+    <Nav pills>
+      <NavItem>
+        <NavLink href="#" active>Link</NavLink>
+      </NavItem>
+      <Dropdown nav {isOpen} toggle={() => (isOpen = !isOpen)}>
+        <DropdownToggle nav caret>Dropdown</DropdownToggle>
+        <DropdownMenu>
+          <DropdownItem header>Header</DropdownItem>
+          <DropdownItem disabled>Action</DropdownItem>
+          <DropdownItem>Another Action</DropdownItem>
+          <DropdownItem divider />
+          <DropdownItem>Another Action</DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
+      <NavItem>
+        <NavLink href="#">Link</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="#">Another Link</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink disabled href="#">Disabled Link</NavLink>
+      </NavItem>
+    </Nav>
+  </div>
+</Story>
+
+<Story name="Underline">
+  <div class="nav-example">
+    <Nav underline>
+      <NavItem>
+        <NavLink href="#" active>Link</NavLink>
+      </NavItem>
+      <Dropdown nav {isOpen} toggle={() => (isOpen = !isOpen)}>
+        <DropdownToggle nav caret>Dropdown</DropdownToggle>
+        <DropdownMenu>
+          <DropdownItem header>Header</DropdownItem>
+          <DropdownItem disabled>Action</DropdownItem>
+          <DropdownItem>Another Action</DropdownItem>
+          <DropdownItem divider />
+          <DropdownItem>Another Action</DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
+      <NavItem>
+        <NavLink href="#">Link</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="#">Another Link</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink disabled href="#">Disabled Link</NavLink>
+      </NavItem>
+    </Nav>
+  </div>
+</Story>
+
+<Story name="Vertical">
+  <div class="nav-example">
+    <p>List Based</p>
+
+    <Nav vertical>
+      <NavItem>
+        <NavLink href="#">Link</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="#">Link</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="#">Another Link</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink disabled href="#">Disabled Link</NavLink>
+      </NavItem>
+    </Nav>
+
+    <hr />
+
+    <p>Link based</p>
+
+    <Nav vertical>
+      <NavLink href="#">Link</NavLink>
+      <NavLink href="#">Link</NavLink>
+      <NavLink href="#">Another Link</NavLink>
+      <NavLink disabled href="#">Disabled Link</NavLink>
+    </Nav>
+  </div>
+</Story>
--- src/Nav/Nav.svelte
+++ src/Nav/Nav.svelte
@@ -1,20 +1,74 @@
 <script>
   import { classnames } from '../utils';
 
+  /**
+   * Additional CSS class names for the <ul> element.
+   * @type {string}
+   */
   let className = '';
   export { className as class };
+
+  /**
+   * Indicates whether the nav should be styled as tabs.
+   * @type {boolean}
+   */
   export let tabs = false;
+
+  /**
+   * Indicates whether the nav should be styled as pills.
+   * @type {boolean}
+   */
   export let pills = false;
-  export let vertical = false;
+
+  /**
+   * Controls vertical orientation of the nav.
+   * @type {string}
+   */
+  export let vertical = '';
+
+  /**
+   * Controls horizontal alignment of the nav items.
+   * @type {string}
+   */
   export let horizontal = '';
+
+  /**
+   * Indicates whether the nav should be justified.
+   * @type {boolean}
+   */
   export let justified = false;
+
+  /**
+   * Indicates whether the nav should fill the available space.
+   * @type {boolean}
+   */
   export let fill = false;
+
+  /**
+   * Indicates whether the nav is part of a navbar.
+   * @type {boolean}
+   */
   export let navbar = false;
+
+  /**
+   * Indicates whether the nav is part of a card header.
+   * @type {boolean}
+   */
   export let card = false;
+
+  /**
+   * Indicates whether the nav should have an underline style.
+   * @type {boolean}
+   */
   export let underline = false;
 
+  /**
+   * Computes the class for vertical orientation based on the provided value.
+   * @param {boolean | 'xs' | 'sm' | 'md' | 'lg' | 'xl'} vertical - The vertical prop value.
+   * @returns {string | false} The class name or false if not applicable.
+   */
   function getVerticalClass(vertical) {
-    if (vertical === false) {
+    if (!vertical) {
       return false;
     }
 
@@ -25,6 +79,10 @@
     return `flex-${vertical}-column`;
   }
 
+  /**
+   * Computed class names for the <ul> element based on the component's props.
+   * @type {string}
+   */
   $: classes = classnames(
     className,
     navbar ? 'navbar-nav' : 'nav',
