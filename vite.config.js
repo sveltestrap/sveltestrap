396c60445a05438ff05c7543ae858704d0fba4b6
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2023-12-26 13:04:31 +1100

    Remove unused config

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,7 @@ import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
-  plugins: [
-    svelte({
-      emitCss: false,
-      compilerOptions: {
-        accessors: true,
-        hydratable: true
-      }
-    })
-  ],
+  plugins: [svelte()],
   test: {
     globals: true,
     environment: 'jsdom',
