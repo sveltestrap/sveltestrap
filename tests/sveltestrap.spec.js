3043b811326bb6bab2c904cb18cef4cd2efbabee
Kieran Boyle <norcal.kieran@gmail.com>
2023-12-26 10:17:41 -0800

    tests(snapshots): use toMatchSnapshot for integration tests

--- tests/sveltestrap.spec.js
+++ tests/sveltestrap.spec.js
@@ -4,29 +4,29 @@ test('components', async ({ page }) => {
   await page.goto('http://localhost:3000');
 
   await expect(page.getByTestId('accordion')).toHaveCount(1);
-  await expect(page.getByTestId('accordion')).toHaveScreenshot(['accordion.png']);
+  expect(await page.getByTestId('accordion').screenshot()).toMatchSnapshot('accordion.png');
 
   await expect(page.getByTestId('alert')).toHaveCount(1);
-  await expect(page.getByTestId('alert')).toHaveScreenshot(['alert.png']);
+  expect(await page.getByTestId('alert').screenshot()).toMatchSnapshot('alert.png');
 
   await expect(page.getByTestId('badge')).toHaveCount(1);
-  await expect(page.getByTestId('badge')).toHaveScreenshot(['badge.png']);
+  expect(await page.getByTestId('badge').screenshot()).toMatchSnapshot('badge.png');
 
   await expect(page.getByTestId('breadcrumb')).toHaveCount(1);
-  await expect(page.getByTestId('breadcrumb')).toHaveScreenshot(['breadcrumb.png']);
+  expect(await page.getByTestId('breadcrumb').screenshot()).toMatchSnapshot('breadcrumb.png');
 
   await expect(page.getByTestId('button')).toHaveCount(1);
-  await expect(page.getByTestId('button')).toHaveScreenshot(['button.png']);
+  expect(await page.getByTestId('button').screenshot()).toMatchSnapshot('button.png');
 
   await expect(page.getByTestId('button-group')).toHaveCount(1);
-  await expect(page.getByTestId('button-group')).toHaveScreenshot(['button-group.png']);
+  expect(await page.getByTestId('button-group').screenshot()).toMatchSnapshot('button-group.png');
 
   await expect(page.getByTestId('button-toolbar')).toHaveCount(1);
-  await expect(page.getByTestId('button-toolbar')).toHaveScreenshot(['button-toolbar.png']);
+  expect(await page.getByTestId('button-toolbar').screenshot()).toMatchSnapshot('button-toolbar.png');
 
   await expect(page.getByTestId('card')).toHaveCount(1);
-  await expect(page.getByTestId('card')).toHaveScreenshot(['card.png']);
+  expect(await page.getByTestId('card').screenshot()).toMatchSnapshot('card.png');
 
   await expect(page.getByTestId('dropdown')).toHaveCount(1);
-  await expect(page.getByTestId('dropdown')).toHaveScreenshot(['dropdown.png']);
+  expect(await page.getByTestId('dropdown').screenshot()).toMatchSnapshot('dropdown.png');
 });
