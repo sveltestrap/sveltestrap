4488262ec3ea713a55330c95a233ac0090e39d06
Kieran Boyle <norcal.kieran@gmail.com>
2024-01-09 13:01:36 -0800

    fix(types): update figure type defs

--- src/Figure/Figure.svelte
+++ src/Figure/Figure.svelte
@@ -4,9 +4,26 @@
 
   setContext('figure', true);
 
+  /**
+   * Additional CSS class names for styling.
+   * @type {string}
+   * @default ''
+   */
   let className = '';
   export { className as class };
+
+  /**
+   * The alt attribute for the image.
+   * @type {string | undefined}
+   * @default undefined
+   */
   export let alt = undefined;
+
+  /**
+   * The caption for the image.
+   * @type {string | HTMLSlotElement | undefined}
+   * @default undefined
+   */
   export let caption = undefined;
 
   $: classes = classnames('figure', className);
