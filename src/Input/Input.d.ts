c7f756e743bf5a852499ad36a5338a9eae9ae3e6
Kieran Boyle <norcal.kieran@gmail.com>
2024-02-04 09:55:31 -0800

    refactor(types): update input event types

--- src/Input/Input.d.ts
+++ src/Input/Input.d.ts
@@ -2,25 +2,35 @@ import { SvelteComponent } from 'svelte';
 import { HTMLInputAttributes, HTMLSelectAttributes, HTMLTextareaAttributes } from 'svelte/elements';
 import { Color, InputType } from '../shared';
 
-export interface InputProps extends HTMLInputAttributes, HTMLSelectAttributes, HTMLTextareaAttributes {
-  bsSize?: 'lg' | 'sm' | string;
-  color?: Color | string;
-  feedback?: string | string[];
-  files?: FileList;
-  group?: any;
-  inner?: HTMLElement;
-  invalid?: boolean;
-  label?: string;
-  plaintext?: boolean;
-  reverse?: boolean;
-  theme?: string;
-  type?: InputType;
-  valid?: boolean;
-}
+type MixedElementProps = HTMLInputAttributes &
+  HTMLSelectAttributes &
+  HTMLTextareaAttributes & {
+    bsSize?: 'lg' | 'sm' | string;
+    color?: Color | string;
+    feedback?: string | string[];
+    files?: FileList;
+    group?: any;
+    inner?: HTMLElement;
+    invalid?: boolean;
+    label?: string;
+    plaintext?: boolean;
+    reverse?: boolean;
+    theme?: string;
+    type?: InputType;
+    valid?: boolean;
+  };
 
-type InputElementEvent<T extends HTMLInputElement, HTMLSelectElement, HTMLTextAreaElement> = Event & {
-  currentTarget: T;
-  target: T;
+type MixedTargetProps = {
+  checked: boolean;
+  valueAsDate: Date;
+  valueAsNumber: number;
+};
+
+export interface InputProps extends MixedElementProps {}
+
+type InputElementEvent<E extends Event = Event> = E & {
+  currentTarget: (HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement) & MixedTargetProps;
+  target: (HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement) & MixedTargetProps;
 };
 
 export interface InputEvents {
