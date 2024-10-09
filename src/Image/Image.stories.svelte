793cf2933d3db09658a041dfbd98174e31ad4b40
Rory Duncan <RoryDuncan@users.noreply.github.com>
2024-10-09 10:26:48 -0700

    Update Image.stories.svelte

--- src/Image/Image.stories.svelte
+++ src/Image/Image.stories.svelte
@@ -1,5 +1,5 @@
 <script context="module">
-  import { Image as Images } from './Image.svelte';
+  import Image from './Image.svelte';
 
   export const meta = {
     title: 'Stories/Images',
@@ -57,7 +57,7 @@
 
 <script>
   import { Story, Template } from '@storybook/addon-svelte-csf';
-  import { Image, Figure } from '@sveltestrap/sveltestrap';
+  import { Figure } from '@sveltestrap/sveltestrap';
 
   const thumbnails = [
     'https://picsum.photos/100/100?random=' + Math.round(Math.random() * 1000),
